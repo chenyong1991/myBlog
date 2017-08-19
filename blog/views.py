9b356e7d65fdd26e573bb6e064b67e06a46d0dfb
nobody <846915433@qq.com>
2017-08-19 10:47:41 +0800

    'update'

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 # Create your views here.
 
 
